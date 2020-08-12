e21d2a1c9ec31982e8dd793e9cf2d018ebb8a24e
Max Ungless <maxungless@protonmail.com>
2020-08-12 13:27:26 +0100

    Update github-sync.sh

--- github-sync.sh
+++ github-sync.sh
@@ -28,8 +28,7 @@ git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHU
 git remote add tmp_upstream "$UPSTREAM_REPO"
 git fetch tmp_upstream
 git remote --verbose
-git checkout master tmp_upstream/master
-echo "Checkout master"
+git pull origin/master
 rm -rf .github
 rm -rf ee/
 git add .

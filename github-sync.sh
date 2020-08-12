fe96bec6e7929497f7916d153ee1142fd9ffbc4f
Max Ungless <maxungless@protonmail.com>
2020-08-12 13:36:18 +0100

    Update github-sync.sh

--- github-sync.sh
+++ github-sync.sh
@@ -25,13 +25,13 @@ echo "BRANCHES=$BRANCH_MAPPING"
 
 git config --unset-all http."https://github.com/".extraheader
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
-git remote add tmp_upstream "$UPSTREAM_REPO"
+git fetch origin
 git pull origin master
-rm -rf .github
-rm -rf ee/
-git add .
+git rm -r .github/
 git commit -m "AUTOSYNC: removed ee/"
 echo "Committed removed directories"
+
+git remote add tmp_upstream "$UPSTREAM_REPO"
 git fetch tmp_upstream
 git remote --verbose
 git push origin "refs/remotes/tmp_upstream/${BRANCH_MAPPING%%:*}:refs/heads/${BRANCH_MAPPING#*:}" --force
